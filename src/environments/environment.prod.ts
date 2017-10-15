7a53e0ef41f83f86aa1b8d148382e4b468f3178e
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-10-15 22:26:53 +0100

    Update apiUrl to new azure app.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   // URL of development API
   //apiUrl: 'https://parking-app-api.herokuapp.com'
-  apiUrl: 'https://parking-app-api.azurewebsites.net'
+  //apiUrl: 'https://parking-app-api.azurewebsites.net'
+  apiUrl: 'http://myparking-app-api.azurewebsites.net'
 };
